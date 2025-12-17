c548a54150a754a4510740a0de188c9a2da9674d
Sirojuz <sirojiddinturopov707@gmail.com>
2025-12-17 17:31:21 +0500

    botstrap

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import VueAxios from "vue-axios";
 
 const app = createApp(App);
 
-axios.defaults.baseURL = "http://localhost:3100"; // Serveringiz uchun
+axios.defaults.baseURL = "https://api.examin.tdmau.uz";
 app.config.globalProperties.$axios = axios;
 
 createApp(App).use(store).use(router).use(VueAxios, axios).mount("#app");
